9f9bcb29cdd0eb4866bec96395ad0b89615d4d2d
heo heo <lopty20@nate.com>
2017-01-31 11:09:06 +0900

    1/31 11"09 heo

--- PASProject/src/main/java/com/app/pas/service/ProjectService.java
+++ PASProject/src/main/java/com/app/pas/service/ProjectService.java
@@ -1,15 +1,23 @@
 package com.app.pas.service;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import com.app.pas.dao.ProjectDao;
+import com.app.pas.dto.ProjectVo;
 
 public class ProjectService {
-	
+
 	private ProjectDao projectDao;
 
 	public void setProjectDao(ProjectDao projectDao) {
 		this.projectDao = projectDao;
 	}
-	
-	
+
+	public List<ProjectVo> getMyProjectById(String mem_Email)
+			throws SQLException {
+		List<ProjectVo> list = projectDao.selectProjectById(mem_Email);
+		return list;
+	}
 
 }
