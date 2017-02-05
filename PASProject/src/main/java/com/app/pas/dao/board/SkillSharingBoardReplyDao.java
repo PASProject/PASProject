049d880dd7184b0912e19658af7d3c2ce43ae075
Park DongSeok <jbs6871@naver.com>
2017-02-05 22:48:11 +0900

    스킬쉐어링게시판 리플라이 하던중 커밋

--- PASProject/src/main/java/com/app/pas/dao/board/SkillSharingBoardReplyDao.java
+++ PASProject/src/main/java/com/app/pas/dao/board/SkillSharingBoardReplyDao.java
@@ -3,17 +3,15 @@ package com.app.pas.dao.board;
 import java.sql.SQLException;
 import java.util.List;
 
-import com.app.pas.dto.board.SkillSharingBoardVo;
+import com.app.pas.dto.board.SkillSharingBoardReplyVo;
+
 
 public interface SkillSharingBoardReplyDao {
 	
-	public List<SkillSharingBoardVo> selectSkillSharingBoardList()throws SQLException;
-	public SkillSharingBoardVo selectSkillSharingBoard(int ssb_Article_Num)throws SQLException;
-	public SkillSharingBoardVo selectSkillSharingBoardTitle(String ssb_Title) throws SQLException;
-	public void insertSkillSharingBoard(SkillSharingBoardVo skillSharingBoardVo)throws SQLException;
-	public void updateSkillSharingBoard(SkillSharingBoardVo skillSharingBoardVo)throws SQLException;
-	public void deleteSkillSharingBoard(int ssb_Article_Num)throws SQLException;
-	public int selectSsbTotalCount() throws SQLException;
-	public List<SkillSharingBoardVo> selectSearchSsbTitle(String ssb_title)throws SQLException;
-	public List<SkillSharingBoardVo> selectSearchSsbTitle()throws SQLException;
+	public List<SkillSharingBoardReplyVo> selectSkillSharingBoardReplyList()throws SQLException;
+	public SkillSharingBoardReplyVo selectSkillSharingBoardReply(int ssb_Article_Num)throws SQLException;
+	public void insertSkillSharingBoardReply(SkillSharingBoardReplyVo skillSharingBoardReplyVo)throws SQLException;
+	public void updateSkillSharingBoardReply(SkillSharingBoardReplyVo skillSharingBoardReplyVo)throws SQLException;
+	public void deleteSkillSharingBoardReply(int ssb_Article_Num)throws SQLException;
+	
 }
---
+++ PASProject/src/main/java/com/app/pas/dao/board/impl/SkillSharingBoardReplyDaoImpl.java
@@ -0,0 +1,57 @@
+package com.app.pas.dao.board.impl;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.app.pas.dao.board.SkillSharingBoardReplyDao;
+import com.app.pas.dto.board.SkillSharingBoardReplyVo;
+import com.app.pas.dto.board.SkillSharingBoardVo;
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+public class SkillSharingBoardReplyDaoImpl implements SkillSharingBoardReplyDao{
+	
+	private SqlMapClient client;
+
+	public void setClient(SqlMapClient client) {
+		this.client = client;
+	}
+	
+	@Override
+	public List<SkillSharingBoardReplyVo> selectSkillSharingBoardReplyList()
+			throws SQLException {
+		List<SkillSharingBoardReplyVo> list = client.queryForList("selectSkillSharingBoardReplyList");
+		return list;
+	}
+
+	@Override
+	public SkillSharingBoardReplyVo selectSkillSharingBoardReply(
+			int ssb_Article_Num) throws SQLException {
+		SkillSharingBoardReplyVo skillSharingBoardReplyVo = 
+				(SkillSharingBoardReplyVo)client.queryForObject("selectSkillSharingBoardReply",ssb_Article_Num);
+		return skillSharingBoardReplyVo;
+	}
+
+	@Override
+	public void insertSkillSharingBoardReply(
+			SkillSharingBoardReplyVo skillSharingBoardReplyVo)
+			throws SQLException {
+		client.insert("insertSkillSharingBoardReply",skillSharingBoardReplyVo);
+		
+	}
+
+	@Override
+	public void updateSkillSharingBoardReply(
+			SkillSharingBoardReplyVo skillSharingBoardReplyVo)
+			throws SQLException {
+		client.update("updateSkillSharingBoardReply",skillSharingBoardReplyVo);
+		
+	}
+
+	@Override
+	public void deleteSkillSharingBoardReply(int ssb_Article_Num)
+			throws SQLException {
+		client.delete("deleteSkillSharingBoardReply",ssb_Article_Num);
+
+	}
+	
+}
