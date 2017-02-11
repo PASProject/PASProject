693a4432f7df4791bc9e642914109d71ccfc64ad
Kim <yomel4@naver.com>
2017-02-11 15:52:12 +0900

    dsf

--- PASProject/src/main/java/com/app/pas/commons/socketjs/WebSocketConfig.java
+++ PASProject/src/main/java/com/app/pas/commons/socketjs/WebSocketConfig.java
@@ -1,9 +1,5 @@
 package com.app.pas.commons.socketjs;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 
 public class WebSocketConfig/* implements WebSocketConfigurer*/ {
