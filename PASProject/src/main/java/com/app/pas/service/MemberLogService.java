5a455587ad47dda8049f8751174c676278a38502
KimHyeongGi <yomel4@naver.com>
2017-02-14 11:07:54 +0900

    asdzz

---
+++ PASProject/src/main/java/com/app/pas/service/MemberLogService.java
@@ -0,0 +1,25 @@
+package com.app.pas.service;
+
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.app.pas.dao.MemberLogDao;
+import com.app.pas.dto.MemberLogVo;
+
+public class MemberLogService {
+
+
+	private MemberLogDao memberLogDao;
+	
+	
+	public void setMemberLogDao(MemberLogDao memberLogDao) {
+		this.memberLogDao = memberLogDao;
+	}
+
+
+	public void insertMemberLog(MemberLogVo memberLogVo)throws SQLException{
+		memberLogDao.insertMemberLog(memberLogVo);
+	}
+	
+}
