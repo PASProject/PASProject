63a88a7d7af8b010080840d4d617274aec41a14c
KimHyeongGi <yomel4@naver.com>
2017-02-17 15:48:57 +0900

    as

--- PASProject/src/main/java/com/app/pas/commons/filter/LoginSessionFilter.java
+++ PASProject/src/main/java/com/app/pas/commons/filter/LoginSessionFilter.java
@@ -18,88 +18,90 @@ import com.app.pas.dto.MemberVo;
 
 public class LoginSessionFilter implements Filter {
 
-	/* private FilterConfig filterConfig; */
-	private List<String> whiteList;
-	private List<String> staticResourceList;
+   /* private FilterConfig filterConfig; */
+   private List<String> whiteList;
+   private List<String> staticResourceList;
+   private static int PROJNUM=1;
 
-	public LoginSessionFilter() {
-		whiteList = new ArrayList<String>();
-		whiteList.add("/pas/index");
-		whiteList.add("/pas/main/loginForm");
+   public LoginSessionFilter() {
+      whiteList = new ArrayList<String>();
+      whiteList.add("/pas/index");
+      whiteList.add("/pas/main/loginForm");
 
-		staticResourceList = new ArrayList<String>();
-		staticResourceList.add("/resource/");
-	}
+      staticResourceList = new ArrayList<String>();
+      staticResourceList.add("/resource/");
+   }
 
-	@Override
-	public void init(FilterConfig filterconfig) throws ServletException {
+   @Override
+   public void init(FilterConfig filterconfig) throws ServletException {
 
-	}
+   }
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response,
-			FilterChain chain) throws IOException, ServletException {
+   @Override
+   public void doFilter(ServletRequest request, ServletResponse response,
+         FilterChain chain) throws IOException, ServletException {
 
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpServletResponse res = (HttpServletResponse) response;
-		HttpSession session = req.getSession();
+      HttpServletRequest req = (HttpServletRequest) request;
+      HttpServletResponse res = (HttpServletResponse) response;
+      HttpSession session = req.getSession();
 
-		String uri = req.getRequestURI();
-		/*
-		 * if(!whiteList.contains(uri)){ boolean isURIResourceFile=false;
-		 * 
-		 * 
-		 * for(String staticResource:staticResourceList){
-		 * if(uri.startsWith(staticResource)){ isURIResourceFile=true; break;
-		 * 
-		 * } }
-		 * 
-		 * if(!isURIResourceFile){ HttpSession session = req.getSession();
-		 * 
-		 * MemberVo member =(MemberVo)session.getAttribute("loginUser");
-		 * if(member ==null){ HttpServletResponse res = (HttpServletResponse)
-		 * response; res.sendRedirect("/pas/index"); return; } }
-		 * 
-		 * }
-		 */
+      String uri = req.getRequestURI();
+      /*
+       * if(!whiteList.contains(uri)){ boolean isURIResourceFile=false;
+       * 
+       * 
+       * for(String staticResource:staticResourceList){
+       * if(uri.startsWith(staticResource)){ isURIResourceFile=true; break;
+       * 
+       * } }
+       * 
+       * if(!isURIResourceFile){ HttpSession session = req.getSession();
+       * 
+       * MemberVo member =(MemberVo)session.getAttribute("loginUser");
+       * if(member ==null){ HttpServletResponse res = (HttpServletResponse)
+       * response; res.sendRedirect("/pas/index"); return; } }
+       * 
+       * }
+       */
 
-		System.out.println(uri + "유알아이!");
+      System.out.println(uri + "유알아이!");
 
-		int proj_Num = 1;
-		int proj_NumN = 1;
+      int proj_Num = 1;
+      int proj_NumN = 1;
 
-		MemberVo member = (MemberVo) session.getAttribute("loginUser");
+      MemberVo member = (MemberVo) session.getAttribute("loginUser");
 
-		if (request.getParameter("proj_Num") == null) {
+      if (request.getParameter("proj_Num") == null) {
+            
+      } else {
+         proj_NumN = Integer.parseInt(request.getParameter("proj_Num"));
+         PROJNUM=proj_NumN; 
 
-		} else {
-			proj_NumN = Integer.parseInt(request.getParameter("proj_Num"));
+      }
 
-		}
-
-		if (session.getAttribute("joinProj") == null) {
+      if (session.getAttribute("joinProj") == null) {
             proj_Num=proj_NumN;
-		} else {
-			proj_Num = Integer.parseInt((String) session
-					.getAttribute("joinProj"));
-		}
-		System.out.println(proj_NumN+"옵니다!");
+      } else {
+         proj_Num = Integer.parseInt((String) session
+               .getAttribute("joinProj"));
+      }
+      System.out.println(proj_NumN+"옵니다!");
 
-		System.out.println(proj_Num + "갑니다!!!");
+      System.out.println(proj_Num + "갑니다!!!");
 
-		if (member == null || proj_Num != proj_NumN) {
-			res.sendRedirect("/pas/index");
-			return;
-		} else {
-			chain.doFilter(req, res);
-		}
+      if (member == null || proj_Num != PROJNUM) {
+         res.sendRedirect("/pas/index");
+         return;
+      } else {
+         chain.doFilter(req, res);
+      }
 
-		chain.doFilter(req, res);
-	}
+      chain.doFilter(req, res);
+   }
 
-	@Override
-	public void destroy() {
+   @Override
+   public void destroy() {
 
-	}
+   }
 
 }
